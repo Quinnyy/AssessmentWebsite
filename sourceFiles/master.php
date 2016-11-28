ec0b10c4e3e551d71d162f5f45839edda77dbf11
Quinnyy <michaelstvns2@gmail.com>
2016-11-28 17:16:34 +0000

    Initial commit

---
+++ sourceFiles/master.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head runat = "server">
+    <link rel="shortcut icon" type="image/x-icon" href="../Images/765-default-avatar.png" />
+    <title>
+        Student Profile
+    </title>
+
+
+    <link href="../CSS/NavbarMaster.css?v={0}" rel="stylesheet" />
+
+</head >
+
+<body class="master-body">
+<form id="form1" runat="server">
+    <header class="header">
+        <nav>
+            <ul>
+                <li><a href="../Web_Forms/HomePage.aspx"><b>Home</b></a></li>
+                <li><a href="../Web_Forms/AboutMe.aspx"><b>About Me</b></a></li>
+                <li><a href="../Web_Forms/FindMe.aspx"><b>Find Me</b></a></li>
+                <li><a href="../Web_Forms/ContactMe.aspx"><b>Contact Me</b></a></li>
+                <li><a href="#"><b>My Modules</b></a>
+                    <ul>
+                        <li><a href="#">Information Technology</a>
+                            <ul>
+                                <li><a href="#">HCI</a></li>
+                                <li><a href="#">Databases</a></li>
+                                <li><a href="#">ASP.net</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Computer Fundamentals</a>
+                            <ul>
+                                <li><a href="#">Linux</a></li>
+                                <li><a href="#">BASH Scripting</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Data Structures</a>
+                            <ul>
+                                <li><a href="#">Java</a></li>
+                                <li><a href="#">C++</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><b>My Experiences</b></a>
+                    <ul>
+                        <li><a href="#">Personal Experience</a></li>
+                        <li><a href="#">Work Experience</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><b>My Projects</b></a></li>
+
+                <li>
+                    <a href="../Web_Forms/Login.aspx"><b>Student Login</b></a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
+</form>
+</body>
+</html>
+
+
+
+
+<?php
+?>
