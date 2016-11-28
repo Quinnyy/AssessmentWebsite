73148fe2185ab6d9d54a67ec7792e4673ad01ab6
Quinnyy <michaelstvns2@gmail.com>
2016-11-28 17:47:13 +0000

    Test

--- sourceFiles/index.php
+++ sourceFiles/index.php
@@ -18,8 +18,8 @@ Discover:
         </p>
         <ul>
             <li> Clubs</li>
-            <li> HealthyLiving</li>
-            <li> LocalTrails </li>
+            <li> Healthy Living</li>
+            <li> Local Trails </li>
         </ul>
     </div>
 </div>
