808acf888641ac0740c2dd3bc8e58efbcf2e8fb9
Quinnyy <michaelstvns2@gmail.com>
2016-12-05 16:37:52 +0000

    Test

--- sourceFiles/contributorPage.php
+++ sourceFiles/contributorPage.php
@@ -3,13 +3,22 @@ include 'master.php';
 ?>
 
 <htm>
-    <link href="master.css" rel="stylesheet" />
-<form action="healthyLiving.php" method="post">
-    Title: <input type="text" name="title">
-    Description: <input type="text" name="desc">
-    <br>
-    <input type ="submit" value = "Submit" name ="submit"
-</form>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Healthy Lifestyle</h1>
+            </div>
+            <form action="healthyLiving.php" method="post">
+                Title: <input type="text" name="title">
+                Description: <input type="text" name="desc">
+                <br>
+                <input type ="submit" value = "Submit" name ="submit"
+            </form>
+        </div>
+
+
+
+
 
 
 </htm>
