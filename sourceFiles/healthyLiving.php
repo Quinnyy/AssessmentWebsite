79f33a8c44128ddc0ffa643928fab79045697939
Quinnyy <michaelstvns2@gmail.com>
2016-12-05 18:56:05 +0000

    Test

--- sourceFiles/healthyLiving.php
+++ sourceFiles/healthyLiving.php
@@ -51,9 +51,7 @@ include 'master.php';
         impact on your day to day life and could even lead to self neglect. Confidence is also a big factor in a healthy lifestyle; having the courage to welcome new
         things into your life can lead to great opportunities.
     </li>
-    <li>
-        <img src="Health-Tips-to-Keep-your-Body-Healthy.jpg">
-    </li>
+
     <ul>
 
 
