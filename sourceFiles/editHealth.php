e81edb7b140e8c0946837aaa5a4dbab9c5fcbf7f
Quinnyy <michaelstvns2@gmail.com>
2016-12-07 17:24:41 +0000

    Test

--- sourceFiles/editHealth.php
+++ sourceFiles/editHealth.php
@@ -5,26 +5,13 @@ $title = $_POST['title'];
 $desc = $_POST['desc'];
 $type = $_POST['type'];
 
-echo $title, $desc, $type;
+
+if($type=="insert")
+{
+    $sql = "INSERT INTO port_articles (title, text) VALUES ('".$title."', '".$desc."')";
+    echo "Article Added, thank you";
+}
 
 ?>
 
 
-
-
-
-
-/* this script inserts a new article into the database*/
-/*
-function insert($title, $text) {
-    $sql = "INSERT INTO port_articles (title, text) VALUES ('".$title."', '".$text."')";
-    echo "The select function is called.";
-    exit;
-}
-
-function edit() {
-    $title = $_POST["title"];
-    $text = $_POST["text"];
-    $sql = "INSERT INTO port_articles (title, text) VALUES ('".$_POST['title']."', '".$_POST['text']."')";
-    exit;
-}
