b31db2d6bec0953331e12f858858d0dc688cd2db
Quinnyy <michaelstvns2@gmail.com>
2016-12-07 17:27:18 +0000

    Test

--- sourceFiles/editHealth.php
+++ sourceFiles/editHealth.php
@@ -5,11 +5,18 @@ $title = $_POST['title'];
 $desc = $_POST['desc'];
 $type = $_POST['type'];
 
-
 if($type=="insert")
 {
-    $sql = "INSERT INTO port_articles (title, text) VALUES ('".$title."', '".$desc."')";
+    $sql = "INSERT INTO port_articles (title, text) VALUES ('".$_POST['title']."', '".$_POST['desc']."')";
     echo "Article Added, thank you";
+
+    if ($db->query($sql) === TRUE) {
+        header( 'Location: http://AssessmentWebsite.azurewebsites.net/sourceFiles/healthyLiving.php' ) ;
+
+    } else {
+        echo "Error: " . $sql . "<br>" . $db->error;
+    }
+
 }
 
 ?>
